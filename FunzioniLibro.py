8580f8ba494e22575344ea6647093b9b3d86d318
Giorgioboh <giorgiocucchi30@gmail.com>
2024-11-28 09:48:17 +0100

    aggiunta di codice

--- FunzioniLibro.py
+++ FunzioniLibro.py
@@ -3,20 +3,36 @@ libriprestati = []
 
 def aggiuntaLibro():
     titolo = input("Inserisci il nome del libro che vuoi inserire: ")
-    catalogo.append(titolo)
-    print("Il libro ", titolo," è stato aggiunto alla libreria.")
+    if titolo in catalogo:
+        print ("Il libro è già presente nel catalogo")
+    elif titolo in libriprestati:
+        print("Il libro è già presente nel nostro catalogo e al momento è in prestito")
+    else:        
+        catalogo.append(titolo)
+        print("Il libro ", titolo," è stato aggiunto alla libreria.")
     
 
 def prestitoLibro():
     titolo = input("Inserisci il titolo del libro che vuoi prendere in prestito: ")
-    catalogo.remove(titolo)
-    libriprestati.append(titolo)
-    print("Il libro ", titolo, " è stato prestato.")
+   
+    if titolo in libriprestati:
+        print("Il libro è stato già prestato")
+    elif titolo in catalogo:
+        catalogo.remove(titolo)
+        libriprestati.append(titolo)
+        print("Il libro ", titolo, " è stato prestato.")
+
     
 def riportaLibro():
     titolo = input("Inserisci il titolo del libro che vuoi riportare: ")
-    catalogo.append(titolo)
-    libriprestati.remove(titolo)
+    if titolo in catalogo:
+        print("Il libro non è stato prestato")
+    elif titolo in libriprestati:
+        catalogo.append(titolo)
+        libriprestati.remove(titolo)
+        print("Il libro è stato riportato")
+    else:
+        print("Il libro non è presente nel nostro catalogo e non è stato prestato")
    
 def disponibilitLibro():
     titolo = input("Inserisci il titolo del libro che che cerchi: ")
